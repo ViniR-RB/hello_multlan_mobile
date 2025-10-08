35ca4adc219dba473c76682ee806eaa74f18e3e0
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-10-08 12:20:34 -0300

    feat: adjust splash page based in designer

--- lib/app/ui/splash/splash_page.dart
+++ lib/app/ui/splash/splash_page.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hello_multlan/app/core/extensions/error_translator.dart';
 import 'package:hello_multlan/app/core/states/command_state.dart';
+import 'package:hello_multlan/app/core/widgets/custom_scaffold_primary.dart';
 import 'package:hello_multlan/app/ui/splash/command/user_logged.dart';
 import 'package:hello_multlan/app/ui/splash/splash_controller.dart';
+import 'package:hello_multlan/gen/assets.gen.dart';
 
 class SplashPage extends StatefulWidget {
   final SplashController _controller;
@@ -20,12 +22,40 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> with ErrorTranslator {
+class _SplashPageState extends State<SplashPage>
+    with ErrorTranslator, TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _opacityAnimation;
+
   @override
   void initState() {
     super.initState();
-    widget._controller.userIsLogged();
+    _initializeAnimation();
     widget._userLogged.addListener(_listernerUserLogged);
+  }
+
+  void _initializeAnimation() {
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    );
+
+    _opacityAnimation =
+        Tween<double>(
+          begin: 0.0,
+          end: 1.0,
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
+        );
+
+    // Inicia a animação e executa userIsLogged durante a animação
+    _animationController.forward().then((_) {
+      // Após a animação completar, executa a verificação
+      widget._controller.userIsLogged();
+    });
   }
 
   void _listernerUserLogged() {
@@ -49,17 +79,25 @@ class _SplashPageState extends State<SplashPage> with ErrorTranslator {
 
   @override
   void dispose() {
+    _animationController.dispose();
     widget._userLogged.removeListener(_listernerUserLogged);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+    return CustomScaffoldPrimary(
+      child: Center(
+        child: AnimatedBuilder(
+          animation: _opacityAnimation,
+          builder: (context, child) {
+            return Opacity(
+              opacity: _opacityAnimation.value,
+              child: Assets.images.logoCompleta.image(width: 200, height: 200),
+            );
+          },
+        ),
       ),
-      body: Container(),
     );
   }
 }
